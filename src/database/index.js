4b1db60052a89b48f19343ec184168f20248bcdc
BRL4528 <blgc.sgo@hotmail.com>
2020-06-20 11:18:25 -0400

    update mongo

--- src/database/index.js
+++ src/database/index.js
@@ -5,24 +5,24 @@ class Database {
     this.mongo();
   }
 
-  //   mongo() {
-  //     this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+  mongo() {
+    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true,
+    });
+  }
+
+  // mongo() {
+  //   this.mongoConnection = mongoose.connect(
+  //     'mongodb://localhost:27017/twobrothers',
+  //     {
   //       useNewUrlParser: true,
   //       useFindAndModify: true,
   //       useUnifiedTopology: true,
-  //     });
-  //   }
+  //     }
+  //   );
   // }
-  mongo() {
-    this.mongoConnection = mongoose.connect(
-      'mongodb://localhost:27017/twobrothers',
-      {
-        useNewUrlParser: true,
-        useFindAndModify: true,
-        useUnifiedTopology: true,
-      }
-    );
-  }
 }
 
 export default new Database();
